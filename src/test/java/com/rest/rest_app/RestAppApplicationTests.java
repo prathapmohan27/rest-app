c5576958ed6bdf9028f25701d9ed699f3c0b92ee
prathapmohan <prathapmohan2704@gamil.com>
2024-10-09 08:27:12 +0530

    Added book and author entity

---
+++ src/test/java/com/rest/rest_app/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.rest_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
